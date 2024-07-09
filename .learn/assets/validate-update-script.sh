064f46b0ba050e83b51f3867aacc88cc63def525
Alejandro Sanchez <aalejo@gmail.com>
2024-07-08 22:09:17 -0400

    Create validate-update-script.sh

---
+++ .learn/assets/validate-update-script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function generar_arreglo_json() {
+  # Verificar si se proporcionó un comando
+  if [ $# -eq 0 ]; then
+    echo "Error: Se debe especificar un comando."
+    return 1
+  fi
+
+  # Ejecutar el comando y capturar la salida
+  comando="$1"
+  salida=$(eval "$comando")
+
+  # Inicializar el arreglo JSON
+  array=()
+
+  # Convertir cada línea de salida en un objeto JSON y agregarlo al arreglo
+  while read -r line; do
+    array+=("\"$line\"")
+  done <<< "$salida"
+
+  # Imprimir el arreglo JSON
+  echo "[$(IFS=','; echo "${array[*]}")]"
+}
+
+# Crontab Info
+comando="crontab -l | grep -v \#"
+crontab_data=$(generar_arreglo_json "$comando");
+
+if [ ! -z "$3" ]; then
+  echo "Crontab data: $crontab_data"
+fi
+
+archivo=$1
+if [ -z $1 ];then
+  # Solicitar la dirección del archivo al usuario
+  echo "Ingrese el nombre del script inluyendo la ruta completa"
+  echo "Type the file name including the full path "
+  read archivo
+  if [ ! -z "$3" ]; then
+    echo "Ruta archivo: $archivo"
+  fi
+fi
+
+# Verificar si el archivo existe
+if [ ! -f "$archivo" ]; then
+  echo "Error: The script '$archivo' doesn't exists."
+  exit 1
+fi
+
+# Leer el contenido del archivo
+contenido_archivo=$(base64 "$archivo")
+if [ ! -z "$3" ]; then
+  echo $contenido_archivo
+fi
+echo "_______________________________________________________"
+
+fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Update data
+comando="stat /var/cache/apt/pkgcache.bin | head -n 6 | tail -n 2"
+update_data=$(generar_arreglo_json "$comando");
+
+
+# Parametros de la petición
+cabeceras="Content-Type: application/json"
+data='{"scriptText":"'$contenido_archivo'","crontabData":'$crontab_data', "scriptFile":"'$archivo'","timestamp":"'$fecha_hora'","updateData":'$update_data'}'
+if [ ! -z "$3" ]; then
+  echo "Data:"
+  echo "$data"
+fi
+echo "_______________________________________________________"
